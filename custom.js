2e0935357168e9526e1d86ec2ef2929244b1c7e3
Jay Palin <jaypalin101@gmail.com>
2016-08-10 11:53:15 -0700

    changed the click function to look at an id instead of class

--- custom.js
+++ custom.js
@@ -9,5 +9,5 @@ $(document).ready(function(){
     $(".quote").text(randomQuote);
     $(".author").text(randomAuthor);
   }
-  $(".btn").on("click", getQuote);
+  $("#newQuote").on("click", getQuote);
 });
