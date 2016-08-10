44fd18fa25b46b2cd93b38dfea4627bb1cbeafb9
Jay Palin <jaypalin101@gmail.com>
2016-08-10 10:26:43 -0700

    linked js and created quote function

--- custom.js
+++ custom.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-
+  function getQuote(){
+    var quotes = ["whatever", "whatever1", "whatever2"];
+    var authors = [" -name", " -name1", " -name2"];
+    var randomNum = Math.floor((Math.random()*quotes.length));
+    var randomQuote = quotes[randomNum];
+    var randomAuthor = authors[randomNum];
+    $(".quote").text(randomQuote);
+    $(".author").text(randomAuthor);
+  }
+  $(".btn").on("click", getQuote);
 });
